d952caeff8b38e9483fb3d36a5a2282b12c83969
RikkRenner <Rikk62@gmail.com>
2021-10-01 10:53:17 +0100

    Revert "initial commit of SC"
    This reverts commit d7ded44da108a63506090eeb484ba2585977c37d.

--- start.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app').app;
-
-const PORT = process.env.PORT || 8080;
-
-// app listener
-app.listen(PORT, () => {
-    console.log(`API Listening on http://localhost:${PORT}`);
-});
